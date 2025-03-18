653811458600a578c24d07ec31c9601a9c65d02b
Mix <32300164+mnixry@users.noreply.github.com>
2025-03-18 10:52:13 +0800

    Refactor build_mksu.sh to streamline patch application and file copying

--- build_mksu.sh
+++ build_mksu.sh
@@ -35,32 +35,21 @@ sed -i 's/ -dirty//g' kernel_platform/build/kernel/kleaf/workspace_status_stamp.
 git clone https://gitlab.com/simonpunk/susfs4ksu.git -b gki-${ANDROID_VERSION}-${KERNEL_VERSION} --depth 1
 write_github_output "susfs_version" $(cat susfs4ksu/ksu_module_susfs/module.prop | sed -n '/version=/ {s/.*=//; p}')
 git clone https://github.com/TanakaLun/kernel_patches4mksu --depth 1
-(
-  cd kernel_platform
-  cp -t KernelSU/ \
-    ../susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch \
-    ../kernel_patches4mksu/mksu/mksu_susfs.patch \
-    ../kernel_patches4mksu/mksu/fix.patch \
-    ../kernel_patches4mksu/mksu/vfs_fix.patch
-  cp -t common/ \
-    ../susfs4ksu/kernel_patches/50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION}.patch
-  cp -t common/fs/ ../susfs4ksu/kernel_patches/fs/*
-  cp -t common/include/linux/ ../susfs4ksu/kernel_patches/include/linux/
-)
 
 # Apply patches
 (
   cd kernel_platform/KernelSU
-  patch -p1 --forward < 10_enable_susfs_for_ksu.patch || true
-  patch -p1 --forward < mksu_susfs.patch || true
-  patch -p1 --forward < fix.patch || true
-  patch -p1 --forward < vfs_fix.patch || true
+  patch -p1 --forward < ../../susfs4ksu/kernel_patches/KernelSU/10_enable_susfs_for_ksu.patch || true
+  patch -p1 --forward < ../../kernel_patches4mksu/mksu/mksu_susfs.patch || true
+  patch -p1 --forward < ../../kernel_patches4mksu/mksu/fix.patch || true
+  patch -p1 --forward < ../../kernel_patches4mksu/mksu/vfs_fix.patch || true
 )
 (
   cd kernel_platform/common
-  patch -p1 < 50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION}.patch || true
-  cp ../../kernel_patches4mksu/69_hide_stuff.patch ./
-  patch -p1 -F 3 < 69_hide_stuff.patch
+  patch -p1 --forward < ../../susfs4ksu/kernel_patches/50_add_susfs_in_gki-${ANDROID_VERSION}-${KERNEL_VERSION}.patch || true
+  patch -p1 --forward < ../../kernel_patches4mksu/69_hide_stuff.patch
+  cp -rv ../../susfs4ksu/kernel_patches/fs/* ./fs/
+  cp -rv ../../susfs4ksu/kernel_patches/include/linux/* ./include/linux/
 )
 
 # Build kernel
